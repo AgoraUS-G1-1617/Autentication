5abe1e9f9f38af430e7ef7fcc839e7a87d71e1c6
fuerte94 <js3_8@hotmail.com>
2016-02-06 21:58:09 +0100

    Modificación pagina de login sin dni

--- auth/loginNotDNIe.php
+++ auth/loginNotDNIe.php
@@ -37,8 +37,8 @@ if (isset($_REQUEST['error'])) {
 	<script type="text/javascript" src="bootstrap/js/index.js"></script>
 	<script type="text/javascript" src="scripts/index.js"></script>
 	
-	<link rel="stylesheet" href="style/style.css" type="text/css">
-
+	
+<link href='https://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
 	
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.css" type="text/css">
 	<link rel="stylesheet" href="styles/bootstrap/css/bootstrap-theme.css" type="text/css">
@@ -48,48 +48,30 @@ if (isset($_REQUEST['error'])) {
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <title><?php echo TITLE?></title>
+    <link rel="stylesheet" href="style/style.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
-<body>
-	
-	<div class="container-fluid">
-		<div class="logo">
-		<h4>Bienvenidos a Agor@us<img alt="Agora@us" src="img/agoraUsImage.jpg" width="100px" height="100px"></h4>
+<body style="font-family: Roboto;">
+	<div class="tituloInicio">
+		<h1 style="font-size: 100px; font-family: Roboto">Entrar sin DNIe</h1>
 	</div>
-<div id="loginWr">
-    <div id="login" >
-        <form action="logAttempt.php" method="post" class="login-form">
-<div class="content">
-                    <label for="user"><img src="img/userSnapshot.png" alt="Nombre de usuario"></label>
-                    <input  type="text" 
-                            id="user" 
-                            name="user" 
-                            style="font-size: 18px"
-                            title="Su nombre de usuario" 
-                            placeholder="Nombre de usuario" />
-                            <br/>
-
-
-                    
- 
-                    <label for="pass"><img src="img/passSnapshot.png" alt="Contraseña" ></label>
-                    <input  type="password" 
-                            id="pass" 
-                            name="pass" 
-                            title="Su contraseña" 
-                            placeholder="Contraseña" 
-                            style="font-size: 18px"/>
-                        </div>
-                            <br />
-                            
-<div class="footer">
-					<input type="submit" class="btn btn-primary" value="Entrar">
-					<input  onClick="location.href = 'register.php' "
-                            id="register" 
-                            type="button"
-                            value ="Registrate" 
-                           	class="btn btn-info"/>
-                           	</div>
-        </form>
+	
+	<div class="formLogin">
+		<form action="logAttempt.php" method="post" >
+		
+		<label for="user"><img src="img/userSnapshot.png" alt="Nombre de usuario"></label>
+        <input  type="text" id="user" name="user" style="font-size: 22px" title="Su nombre de usuario" placeholder="Nombre de usuario" />
+        
+        <label for="pass" style="margin-left: 30px;"><img src="img/passSnapshot.png" alt="Contraseña" ></label>
+        <input type="password" id="pass" name="pass" title="Su contraseña" placeholder="Contraseña" style="font-size: 22px"/>
+        <br />
+        <br />
+        <br />
+        <br />
+        
+        <input type="submit" class="btn btn-info" value="Entrar">
+                          
+	</div>
     <div id="error"><?php echo $errorMsg?></div>
     </div>
 </div>
