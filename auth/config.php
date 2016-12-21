154a593c052e5906c757365d7b05e5b372d815fb
Julio <corxonero@gmail.com>
2016-12-21 11:13:10 +0100

    Autenticacion con Twitter

---
+++ auth/config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY', 'TxDVYignMmYI1g27O358Dz3q1');
+define('CONSUMER_SECRET', 'xYDio3lTps1OSXXhHebikt13OKZ5zb6oSJffO8ybCrSC05v2AJ');
+define('OAUTH_CALLBACK', 'http://127.0.0.1/git/Autentication/auth/welcome.php');
+?>
