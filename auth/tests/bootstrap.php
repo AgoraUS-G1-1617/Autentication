7d57c2eb81f47032bef13db0c4aae457e20ba76c
Alejandro Garrido Resina <agresina@gmail.com>
2017-01-10 18:05:15 +0100

    Update bootstrap.php

--- auth/tests/bootstrap.php
+++ auth/tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . 'databaseTest.php';
-require_once dirname(__FILE__) . 'tokenTest.php';
+require_once dirname(__FILE__) . '\databaseTest.php';
+require_once dirname(__FILE__) . '\tokenTest.php';
 date_default_timezone_set('UTC');
 ?>
