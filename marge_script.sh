d78f7c5d18ebbb99acf3e2869681bbe99182cc08
aleromrod1 <alex_sfc4@hotmail.com>
2017-01-31 19:54:57 +0100

    Cambio de prueba en travis.yml

--- marge_script.sh
+++ marge_script.sh
@@ -2,8 +2,10 @@ if [ "$TRAVIS_BRANCH" != "test" ]; then
     exit 0;
 fi
 
-export GIT_COMMITTER_EMAIL=alex_sfc4@hotmail.com
-export GIT_COMMITTER_NAME="aleromrod1"
+export GIT_AUTHOR_NAME="corxonero"
+export GIT_AUTHOR_EMAIL=corxonero@gmail.com
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
 git checkout master || exit
 git merge "$TRAVIS_COMMIT" || exit
